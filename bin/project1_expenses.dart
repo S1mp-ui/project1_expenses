f52d39036319e852ca01aa12b2a79c7648fe63ea
Yutthawee <6631501097@lamduan.mfu.ac.th>
2025-08-22 17:01:41 +0700

    intial commit

---
+++ bin/project1_expenses.dart
@@ -0,0 +1,5 @@
+import 'package:project1_expenses/project1_expenses.dart' as project1_expenses;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project1_expenses.calculate()}!');
+}
---
+++ test/project1_expenses_test.dart
@@ -0,0 +1,8 @@
+import 'package:project1_expenses/project1_expenses.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
