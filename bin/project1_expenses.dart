66b6428fb49af68a02307014e5a16e26475d4410
Yutthawee <6631501097@lamduan.mfu.ac.th>
2025-08-22 17:25:10 +0700

    For All Frontend

--- bin/project1_expenses.dart
+++ bin/project1_expenses.dart
@@ -1,3 +1,39 @@
-void main(){
-  
-}
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const con = require('./db');
+
+
+app.use(express.json()); // รองรับ JSON
+app.use(express.urlencoded({ extended: true }));
+
+
+//=======LOGIN========
+
+
+
+//----Get all passwords----- for storing user_id
+
+
+
+
+//=======Get all expenses========
+
+
+
+//=======Get today expenses======
+
+
+//========Serch expenses=======
+
+
+
+//=======Add expenses========
+
+
+
+
+//=======Delete expenses========
+
+
+
