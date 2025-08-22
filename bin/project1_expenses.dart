6fa5da756c94216094512b06a16d3af5c9d6c85e
Yutthawee <6631501097@lamduan.mfu.ac.th>
2025-08-22 17:06:54 +0700

    Main

--- bin/project1_expenses.dart
+++ bin/project1_expenses.dart
@@ -1,5 +1,3 @@
-import 'package:project1_expenses/project1_expenses.dart' as project1_expenses;
-
-void main(List<String> arguments) {
-  print('Hello world: ${project1_expenses.calculate()}!');
+void main(){
+  
 }
